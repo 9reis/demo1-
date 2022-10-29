e6434e9b71c2419231c83028874870bc8f89899b
Lucas Reis <lucasreis_9@hotmail.com>
2022-10-29 14:11:47 -0300

    Projeto Spring boot inicializado

---
+++ demo1/src/main/java/com/example/demo1/Demo1Application.java
@@ -0,0 +1,21 @@
+package com.example.demo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Demo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo1Application.class, args);
+	}
+	
+	@GetMapping("/inicio")
+	public String inicio( ) {
+		return "<h1> Projeto Spring iniciado </h1>";
+	}
+
+}
